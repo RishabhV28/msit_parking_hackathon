3348e7f0278475bb1799f674b568d35b807d13f4
Rishabh Verma <114663677+mintpapa@users.noreply.github.com>
2025-02-18 10:24:27 +0530

    backend

--- backend2/backend.py
+++ backend2/backend.py
@@ -9,7 +9,6 @@ def get_parking_data():
         data = json.load(f)
     return {"spots": data}
 
-# Run server
 if _name_ == "_main_":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
